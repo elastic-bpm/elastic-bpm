1339e3b7eb76aa47e27765287499aa0741f9207a
Johannes Bertens <johannes.bertens@live.nl>
2016-10-08 22:23:41 +0200

    Using spawn and azure CLI now

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const subscriptionId = '849bd443-6f96-44ae-bcc9-e5295698e1f9';
-const resourceGroupName = 'dockerswarmrg303651';
-const labName = 'DockerSwarm';
+const subscriptionId = process.env.subscriptionId;
+const resourceGroupName = process.env.resourceGroupName;
+const labName = process.env.labName;
 
- // Interactive Login
- // It provides a url and code that needs to be copied and pasted in a browser and authenticated over there. If successful, 
- // the user will get a DeviceTokenCredentials object.
-var msRestAzure = require('ms-rest-azure');
-var resourceManager = require('azure-arm-resource');
-var util = require('util');
-msRestAzure.interactiveLogin(function(err, credentials, subscriptions) {
-  if (err) {
-     console.log(err);
-     return;
-  }
-  console.log('Credentials object:\n' + util.inspect(credentials, {depth: null }));
-  console.log('List of subscriptions:\n' + util.inspect(subscriptions, {depth: null }));
-  var client = new resourceManager.ResourceManagementClient(credentials, subscriptionId);
-  client.resourceGroups.list(function (err, result, request, response) {
- if (err) {
-    console.log(err);
-  } else {
-    console.log('>>>List of ResourceGroups:');
-    console.log(result);
-  }
- });
+var spawn = require('cross-spawn');
+
+const login = spawn('azure', ['login']);
+login.stdout.on('data', (data) => { process.stdout.write(data);});
+login.stderr.on('data', (data) => { process.stdout.write(data);});
+
+login.on('close', (code) => {
+  const set_account = spawn('azure',['account','set', '4']);
+  set_account.stdout.on('data', (data2) => { process.stdout.write(data2);});
+  set_account.stderr.on('data', (data2) => { process.stdout.write(data2);});
+
+  set_account.on('close', (code2) => {
+    const list_vms = spawn('azure',['vm','list', '--json']);
+    list_vms.stdout.on('data', (data3) => { 
+      // process.stdout.write(data3);
+      vms = JSON.parse(data3);
+      // console.dir(vms);
+      vms.forEach(function(element) {
+        console.log("Name: " + element.name + ", powerState: " + element.powerState);
+      }, this);
+    });
+    list_vms.stderr.on('data', (data3) => { process.stdout.write(data3);});
+
+    list_vms.on('close', (code3) => {
+      console.log("Exit!");
+    });
+  });
 });
