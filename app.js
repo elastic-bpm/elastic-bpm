c41bb16c07e78458477908c3bfcbf6a97c8ecbb3
Johannes Bertens <johannes.bertens@live.nl>
2016-10-15 21:19:50 +0200

    Added socket.io

--- app.js
+++ app.js
@@ -1,31 +1,56 @@
+/*jshint esversion: 6 */
+
 var bodyParser = require('body-parser');
 var express = require('express'),
     app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+    
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-get_event = function (req, res) {
-    res.setHeader('Content-Type', 'application/json'); 
-    res.send({
+var num_events = 0;
+var events = [];
+generate_event = function() {
+    num_events++;
+    event = {
         time: (new Date()).toJSON(),
-        message: "Event message",
-    }); 
+        message: "Event message " + num_events,
+    };
+    events.push(event); 
+    return event;
 };
+
+send_event = function () {
+    io.emit('event', generate_event());
+};
+
+// Send new connections EVERYTHING... muhahaha
+io.on('connect', (socket) => {
+    events.forEach((event) => {
+        socket.emit('event', event);
+    });
+});
 
 // ROUTING
 setup_routes = function() {
-   app.get('/event', get_event);
+//   app.get('/event', get_event);
+};
+
+// Emit events
+start_casting = function () {
+    send_event();
+    setTimeout(start_casting, 2000);
 };
 
 // Server startup
 start_server = function() {
-    app.listen(3000, function () {
-        console.log('Elastic-dashboard listening on port 3000!');
-    });
+    server.listen(3000, () => console.log('Elastic-dashboard listening on port 3000!'));
 };
 
 // When run directly, serve the API
 if (require.main === module) {
     setup_routes();
     start_server();
+    start_casting();
 }
