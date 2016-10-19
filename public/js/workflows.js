b7a43a701ce132cf4777ad1d34fcd99ad3541f1a
Johannes Bertens <johannes.bertens@live.nl>
2016-10-19 22:29:26 +0200

    workflow display not working yet

--- public/js/workflows.js
+++ public/js/workflows.js
@@ -8,18 +8,26 @@ workflows_init = function (socket, interval) {
 $.addTemplateFormatter({
     WorkflowDeleteButton: function(value, template) {
         return "delete-workflow-" + value + "-button";
+    },
+    WorkflowModal: function(value, template) {
+        if (template) {
+            return template + value + "-modal";
+        } else {
+            return value + "-modal";
+        }
     }
 });
 
 create_workflow = function() {
     var name = $("#workflow-name").val();
     var owner = $("#workflow-owner").val();
-    var info = $("#workflow-info").val();
+    var edges = $("#workflow-edges").val();
+    var nodes = $("#workflow-nodes").val();
 
     // Elaborate post, because I want to use the body
     $.ajax({
         contentType: 'application/json',
-        data: JSON.stringify({"name": name, "owner": owner, "info": info}),
+        data: JSON.stringify({"name": name, "owner": owner, "edges": edges, "nodes": nodes}),
         success: function(data){
             console.log(data);
             $('#workflow-create-form').trigger('reset');
@@ -48,6 +56,7 @@ delete_workflow = function(workflow_id) {
 };
 
 init_graph = function(elementId, nodes, edges) {
+    console.log(elementId);
     var cy = window.cy = cytoscape({
         container: document.getElementById(elementId),
 
@@ -86,32 +95,44 @@ init_graph = function(elementId, nodes, edges) {
 };
 
 workflow_template_shown = false;
+fill_template = function (workflows) {
+    $("#workflows-modal").loadTemplate("templates/workflow-graph-template.html", workflows, {success: () => {
+        nodes = [
+            { data: { id: 'n0' } },
+            { data: { id: 'n1' } },
+            { data: { id: 'n2' } },
+            { data: { id: 'n3' } }
+        ];
+        edges = [
+            { data: { source: 'n0', target: 'n1' } },
+            { data: { source: 'n1', target: 'n2' } },
+            { data: { source: 'n1', target: 'n3' } }
+        ];
+
+        workflows.forEach((item) => {
+            init_graph('cy', nodes, edges);
+        }); 
+
+        $("#workflows-table-body").loadTemplate("templates/workflows-template.html", workflows, {success: () => {
+            workflows.forEach((item) => {
+                $("#delete-workflow-" + item.id + "-button").on('click', (event) => delete_workflow(item.id));
+            });
+        }});
+
+    }});
+};
+
 show_workflows = function() {
     $.get('/workflows', (workflows) => {
         if (!workflow_template_shown) {
             $.get("/parts/workflows.html", (data) => {
                 $("#workflows").html(data);
                 $("#workflow-create-button").on('click', create_workflow);
-                workflow_template_shown = true;       
+                workflow_template_shown = true;
+                fill_template(workflows);       
             });
         } else {
-            $("#workflows-table-body").loadTemplate("templates/workflows-template.html", workflows);
-            workflows.forEach((item) => {
-                $("#delete-workflow-" + item.id + "-button").on('click', (event) => delete_workflow(item.id));
-            });
-
-            nodes = [
-                { data: { id: 'n0' } },
-                { data: { id: 'n1' } },
-                { data: { id: 'n2' } },
-                { data: { id: 'n3' } }
-            ];
-            edges = [
-                { data: { source: 'n0', target: 'n1' } },
-                { data: { source: 'n1', target: 'n2' } },
-                { data: { source: 'n1', target: 'n3' } }
-            ];
-            init_graph('cy', nodes, edges);
+            fill_template(workflows);
         }
     }).fail(() => {
         $.get("/parts/workflows_error.html", (data) => {
