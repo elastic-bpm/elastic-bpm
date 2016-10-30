dd51c808619281db5224a6deb1c9e6b13c093ba6
Johannes Bertens <johannes.bertens@live.nl>
2016-10-30 12:49:34 +0100

    Moved back to npm, faster on local cache

--- app.js
+++ app.js
@@ -1,5 +1,37 @@
 /*jshint esversion: 6 */
 
 var Docker = require('dockerode');
+var docker = new Docker();
 
-console.log("Hello, docker");
+var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express();
+app.use(bodyParser.json());
+
+get_containers = function(req, res) {
+    docker.listContainers(function (err, containers) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify(containers, null, 3));
+        }
+    });
+};
+
+// ROUTING
+setup_routes = function() {
+    app.get('/containers', get_containers); 
+    app.get('/status', (req, res) => res.send('ok'));
+};
+
+// Server startup
+start_server = function() {
+    app.listen(4444, () => console.log('elastic-docker listening on port 4444!'));
+};
+
+// When run directly, serve the API
+if (require.main === module) {
+    setup_routes();
+    start_server();
+}
