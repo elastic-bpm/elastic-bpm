d066afff5ac7bb8f4efc983c8732633bca142e4e
Johannes Bertens <johannes.bertens@live.nl>
2016-10-30 21:38:37 +0100

    Updated workers

--- public/js/workers.js
+++ public/js/workers.js
@@ -13,10 +13,19 @@ show_workers_table = function () {
     global_status_data.forEach((item) => {
         if (item.name === 'elastic-docker' && item.status === 200) {
             $("#workers-info").html("");
+            fill_workers();
         } else if (item.name === 'elastic-docker'){
             $.get("/parts/workers_error.html", (data) => {
                 $("#workers-info").html(data);
             });
         }
+    });
+};
+
+fill_workers = function() {
+ $.get("/workers", (data) => {
+        $("#workers-table-body").loadTemplate("templates/workers-template.html", data);
+    }).fail(() => {
+        console.log("unable to get remote workers data");
     });
 };
