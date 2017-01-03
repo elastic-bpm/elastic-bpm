93868f1f3ca94ff15751c1a17757156841c7b6eb
DJBnJack <johannes.bertens@gmail.com>
2017-01-03 17:32:59 +0100

    Better info

--- stats/stats.js
+++ stats/stats.js
@@ -17,23 +17,23 @@ var stats_module = (function (task_repository, moment) {
         task_start[JSON.stringify(task)] = moment().toJSON();
     };
 
-    var get_time_from_list = function(task_list, node) {
+    var get_time_from_list = function(task_list, node, workflow_id) {
         var time = -1;
         Object.keys(task_list).forEach(function(key, index) {
             var task = JSON.parse(key);
-            if (task.task_id === node) {
+            if (task.task_id === node && task.workflow_id === workflow_id) {
                 time = task_list[key];
             }
         });
         return time;
     };
 
-    var get_start_time = function(node) {
-        return get_time_from_list(task_start, node);
+    var get_start_time = function(node, workflow_id) {
+        return get_time_from_list(task_start, node, workflow_id);
     };
 
-    var get_finish_time = function(node) {
-        return get_time_from_list(task_done, node);
+    var get_finish_time = function(node, workflow_id) {
+        return get_time_from_list(task_done, node, workflow_id);
     };
 
     var remove_nodes_from_lists = function(workflow_id) {
@@ -52,6 +52,38 @@ var stats_module = (function (task_repository, moment) {
         }, task_start);
     };
 
+    var get_previous_tasks = function(task, edges_string) {
+        previous_tasks = [];
+
+        edge_words = edges_string.split(",").map(w => w.trim());
+        edge_words.forEach((word) => {
+            elements = word.split("->").map(w => w.trim());
+            if (task === elements[1]) {
+                previous_tasks.push(elements[0]);
+            }
+        });
+
+        return previous_tasks;
+    };
+
+    var fill_ready_time = function(nodes_info, workflow) {
+        nodes_info.forEach(function(node_info) {
+            var previous_tasks = get_previous_tasks(node_info.node, workflow.edges);
+            console.log("Pevious tasks: " + JSON.stringify(previous_tasks));
+            node_info.ready_to_start = workflow.created;
+
+            previous_tasks.forEach(function(previous_task) {
+                var prev_time = get_finish_time(previous_task, workflow.id);
+                console.log("Found prev_time: " + prev_time);
+                if (moment(prev_time).isAfter(moment(node_info.ready_to_start))) {
+                    node_info.ready_to_start = prev_time;
+                }
+            });
+        });
+
+        return nodes_info;
+    };
+
     var print_stats = function() {
         var ntasks_start = Object.keys(task_start).length;
         var ntasks_done = Object.keys(task_done).length;
@@ -67,11 +99,12 @@ var stats_module = (function (task_repository, moment) {
                         nodes.forEach(function(node) {
                             nodes_info.push({
                                 node: node,
-                                node_created: workflow.created,
-                                node_started: get_start_time(node),
-                                node_finished: get_finish_time(node)
+                                created: workflow.created,
+                                started: get_start_time(node, workflow.id),
+                                finished: get_finish_time(node, workflow.id)
                             });
                         });
+                        nodes_info = fill_ready_time(nodes_info, workflow);
 
                         finished_workflows.push(workflow.id);
                         remove_nodes_from_lists(workflow.id);
