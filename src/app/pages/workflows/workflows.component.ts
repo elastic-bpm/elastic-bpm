2230039fca460be2e85b7bbde30c29b7f2029d86
DJBnJack <johannes.bertens@gmail.com>
2017-03-08 21:47:20 +0100

    Added the graph, yes!

--- src/app/pages/workflows/workflows.component.ts
+++ src/app/pages/workflows/workflows.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { WorkflowService } from '../../services/workflow.service';
 
-import * as cytoscape from 'cytoscape';
+import * as dagre from '../../../../node_modules/dagre/dist/dagre';
+import * as cytoscape from '../../../../node_modules/cytoscape/dist/cytoscape';
+import * as cytoscape_dagre from '../../../../node_modules/cytoscape-dagre/cytoscape-dagre';
 
 const testFlow = {
   name: 'Test-Workflow',
@@ -24,6 +26,7 @@ export class WorkflowsComponent implements OnInit {
 
   ngOnInit() {
     this.workflowService.workflows.subscribe(workflows => this.workflows = workflows);
+    cytoscape_dagre(cytoscape, dagre);
   }
 
 
@@ -72,24 +75,103 @@ export class WorkflowsComponent implements OnInit {
     this.selectedWorkflow = this.workflows.find((wf) => wf.id === id);
   }
 
+  get_nodes = function (node_string, busy, done) {
+    const nodes = [];
+    const node_words = node_string.split(',').map(w => w.trim());
+
+    node_words.forEach((word) => {
+      let classes = '';
+      if (busy.indexOf(word) > -1) {
+        classes = 'busy';
+      }
+
+      if (done.indexOf(word) > -1) {
+        classes = 'done';
+      }
+
+      const node = {
+        data: { id: word },
+        classes: classes,
+      };
+      nodes.push(node);
+    });
+
+    return nodes;
+  };
+
+  get_edges = function (edges_string) {
+    const edges = [];
+    const edge_words = edges_string.split(',').map(w => w.trim());
+
+    edge_words.forEach((word) => {
+      const elements = word.split('->').map(w => w.trim());
+
+      edges.push({ data: { source: elements[0], target: elements[1] } });
+    });
+
+    return edges;
+  };
+
   showGraph() {
     const container = document.getElementById('cy');
     if (container === null) {
       setTimeout(() => this.showGraph(), 100);
     } else {
-      const options: Cy.CytoscapeOptions = {
+      const edges = this.get_edges(this.selectedWorkflow['edges']);
+      const nodes = this.get_nodes(
+        this.selectedWorkflow['nodes'],
+        this.selectedWorkflow['busy_nodes'],
+        this.selectedWorkflow['done_nodes']
+      );
+
+      const options = {
         container: container,
-        elements: [ // list of graph elements to start with
-          { // node a
-            data: { id: 'a' }
+        boxSelectionEnabled: false,
+        autounselectify: true,
+        layout: { name: 'dagre' },
+        style: [
+          {
+            selector: 'node',
+            style: {
+              'content': 'data(id)',
+              'text-opacity': 0.5,
+              'text-valign': 'center',
+              'text-halign': 'right',
+              'background-color': '#11479e'
+            }
           },
-          { // node b
-            data: { id: 'b' }
+          {
+            selector: '.busy',
+            style: {
+              'content': 'data(id)',
+              'text-opacity': 0.5,
+              'text-valign': 'center',
+              'text-halign': 'right',
+              'background-color': '#FF479e'
+            }
           },
-          { // edge ab
-            data: { id: 'ab', source: 'a', target: 'b' }
+          {
+            selector: '.done',
+            style: {
+              'content': 'data(id)',
+              'text-opacity': 0.5,
+              'text-valign': 'center',
+              'text-halign': 'right',
+              'background-color': '#11FF9e'
+            }
+          },
+          {
+            selector: 'edge',
+            style: {
+              'width': 4,
+              'target-arrow-shape': 'triangle',
+              'line-color': '#9dbaea',
+              'target-arrow-color': '#9dbaea',
+              'curve-style': 'bezier'
+            }
           }
         ],
+        elements: { nodes: nodes, edges: edges },
       };
       const cy = cytoscape(options);
     }
