1e92ada7032a1b69d664ceacecc39db294a07beb
DJBnJack <johannes.bertens@gmail.com>
2017-03-12 17:13:30 +0100

    Getting load from ELK

--- components/elastic.js
+++ components/elastic.js
@@ -10,21 +10,120 @@ elastic_component = (function () {
 
     var component = {};
     var messages = [];
+    var rawLoad = [];
+    var maxLoadLength = 5;
+
     var status = {
         message: "not updated yet",
         statusCode: 500
     };
-    var machineLoad = {
-        'node-01': {
-            load1: [0.7, 0.8, 2],
-            load5: [0.7, 0.8, 1],
-            load15: [0.7, 0.8, 1]
-        }
-    };
+    var machineLoad = {};
 
     component.start_updates = function (interval) {
         update_status(interval);
         update_messages(interval);
+        update_load(interval);
+    }
+
+    var update_load = function (interval) {
+        var newLoad = [];
+
+        var from = startTime;
+        if (rawLoad.length > 0) {
+            from = new Date(rawLoad[0]['@timestamp']).getTime();
+        }
+
+        var query = {
+            index: 'metricbeat-*',
+            scroll: '30s',
+            body: {
+                "query": {
+                    "bool": {
+                        "must": [
+                            {
+                                "query_string": {
+                                    "analyze_wildcard": true,
+                                    "query": "*"
+                                }
+                            },
+                            {
+                                "match": {
+                                    "metricset.name": {
+                                        "query": "load",
+                                        "type": "phrase"
+                                    }
+                                }
+                            },
+                            {
+                                "range": {
+                                    "@timestamp": {
+                                        "gt": from,
+                                        "format": "epoch_millis"
+                                    }
+                                }
+                            }
+                        ],
+                        "must_not": []
+                    }
+                }
+            }
+        };
+
+        client.search(query, function getMoreUntilDone(error, response) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.hits.hits.forEach(function (hit) {
+                    newLoad.push(hit._source);
+                });
+                if (response.hits.total > newLoad.length) {
+                    client.scroll({
+                        scrollId: response._scroll_id,
+                        scroll: '30s'
+                    }, getMoreUntilDone);
+                } else {
+                    newLoad.sort(function (a, b) {
+                        if (a["@timestamp"] < b["@timestamp"]) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    })
+
+
+                    newLoad.forEach(function(load) {
+                        if (Object.keys(machineLoad).indexOf(load.beat.hostname) !== -1) {                            
+                            machineLoad[load.beat.hostname].load1.push(load.system.load["1"]);
+                            if (machineLoad[load.beat.hostname].load1.length > maxLoadLength) {
+                                machineLoad[load.beat.hostname].load1 = machineLoad[load.beat.hostname].load1.slice(1);
+                            }
+
+                            machineLoad[load.beat.hostname].load5.push(load.system.load["5"]);
+                            if (machineLoad[load.beat.hostname].load5.length > maxLoadLength) {
+                                machineLoad[load.beat.hostname].load5 = machineLoad[load.beat.hostname].load5.slice(1);
+                            }
+
+                            machineLoad[load.beat.hostname].load15.push(load.system.load["15"]);
+                            if (machineLoad[load.beat.hostname].load15.length > maxLoadLength) {
+                                machineLoad[load.beat.hostname].load15 = machineLoad[load.beat.hostname].load15.slice(1);
+                            }
+                        } else {
+                            machineLoad[load.beat.hostname] = {
+                                load1: [load.system.load["1"]],
+                                load5: [load.system.load["5"]],
+                                load15: [load.system.load["15"]],
+                            }
+                        }
+                    })
+
+                    if (newLoad.lenth > 0) {
+                        rawLoad = newLoad;
+                    }
+
+                    setTimeout(() => { update_load(interval) }, interval);
+                }
+            }
+        });
     }
 
     var update_messages = function (interval) {
@@ -118,7 +217,7 @@ elastic_component = (function () {
         return messages;
     }
 
-    component.get_load = function() {
+    component.get_load = function () {
         return machineLoad;
     }
 
