ea4d5f1a9613190cf990223d87b774da9997a1f4
DJBnJack <johannes.bertens@gmail.com>
2017-03-13 20:27:58 +0100

    Added gelf logging and 3210 default port

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import * as http from 'http';
 import * as debug from 'debug';
+import * as os from 'os';
+import * as log4js from 'log4js';
 import 'source-map-support/register';
 
 import App from './App';
 
 debug('ts-express:server');
+log4js.configure({
+  appenders: [
+    { type: 'console' },
+    {
+      type: 'gelf',
+      host: '137.116.195.67',
+      port: 12201,
+      hostname: 'elastic-scheduler@' + os.hostname(),
+      layout: {
+        type: 'pattern',
+        pattern: '%m'
+      }
+    }],
+  replaceConsole: true
+});
 
-const port = normalizePort(process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || 3210);
 App.set('port', port);
 
 const server = http.createServer(App);
@@ -14,7 +31,7 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-function normalizePort(val: number|string): number|string|boolean {
+function normalizePort(val: number | string): number | string | boolean {
   const localPort: number = (typeof val === 'string') ? parseInt(val, 10) : val;
   if (isNaN(localPort)) {
     return val;
